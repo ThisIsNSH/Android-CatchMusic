0b9bc2a65bdd849ba024c4a42830a07788711cf5
ThisIsNSH <hadanis.singh@gmail.com>
2018-06-01 21:55:32 +0530

    api enabled

--- app/src/main/java/com/nsh/catchmusic/MainActivity.java
+++ app/src/main/java/com/nsh/catchmusic/MainActivity.java
@@ -1,138 +1,95 @@
 package com.nsh.catchmusic;
 
-import android.Manifest;
-import android.content.ComponentName;
-import android.content.Intent;
-import android.content.ServiceConnection;
-import android.content.pm.PackageManager;
+import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
-import android.content.res.Resources;
-import android.media.MediaRecorder;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.Handler;
-import android.os.IBinder;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import com.android.volley.Cache;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Network;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.api.gax.core.CredentialsProvider;
+import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.api.gax.core.GoogleCredentialsProvider;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.speech.v1.RecognitionAudio;
+import com.google.cloud.speech.v1.RecognitionConfig;
+import com.google.cloud.speech.v1.RecognizeResponse;
+import com.google.cloud.speech.v1.SpeechClient;
+import com.google.cloud.speech.v1.SpeechRecognitionAlternative;
+import com.google.cloud.speech.v1.SpeechRecognitionResult;
+import com.google.cloud.speech.v1.SpeechSettings;
+import com.google.protobuf.ByteString;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.File;
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Random;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
-import cafe.adriel.androidaudioconverter.AndroidAudioConverter;
-import cafe.adriel.androidaudioconverter.callback.IConvertCallback;
-import cafe.adriel.androidaudioconverter.model.AudioFormat;
-
-import static android.Manifest.permission.RECORD_AUDIO;
-import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
-
+import static com.google.auth.oauth2.ClientId.fromStream;
 
 public class MainActivity extends AppCompatActivity {
-    String wer;
-    String query;
-    String AudioSavePathInDevice = null;
-    MediaRecorder mediaRecorder;
-    Random random;
-    String RandomAudioFileName = "ABCDEFGHIJKLMNOP";
-    public static final int RequestPermissionCode = 1;
+    JSONObject jsonRequest;
     int check = 0;
     RequestQueue queue;
-    String get_track, get_lyrics, lyrics, get_track_new, get_lyrics_new;
-    TextView user_lyrics;
+    String api_key, get_track, get_lyrics, lyrics, get_track_new, get_lyrics_new;
+
     Button find, btnSpeak;
-    TextView dis_lyrics, dis_track;
+    TextView user_lyrics, dis_lyrics, dis_track;
     long track_id;
-    File finalFile;
+
     JsonObjectRequest lyricsRequest;
-    FileInputStream input;
     AssetManager assetManager;
-    File nsh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        shot();
         find = findViewById(R.id.find);
         dis_lyrics = findViewById(R.id.lyrics1);
         dis_track = findViewById(R.id.track);
         user_lyrics = findViewById(R.id.lyrics);
+
         assetManager = this.getAssets();
+
         btnSpeak = findViewById(R.id.btnSpeak);
-
-        Utils.requestPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
-        Utils.requestPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-
-
-        btnSpeak.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(final View v) {
-                if (checkPermission()) {
-                    AudioSavePathInDevice =
-                            Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + "AudioRecording.mp3";
-                    Log.i("audio save path ", AudioSavePathInDevice);
-                    nsh = new File(AudioSavePathInDevice);
-                    MediaRecorderReady();
-                    try {
-                        mediaRecorder.prepare();
-                        mediaRecorder.start();
-                        final Handler handler = new Handler();
-                        handler.postDelayed(new Runnable() {
-                            @Override
-                            public void run() {
-                                Log.i("recorder", "run: stopeed");
-                                mediaRecorder.stop();
-
-                            }
-                        }, 5000);
-                    } catch (IllegalStateException e) {
-                        e.printStackTrace();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                } else {
-                    requestPermission();
-                }
-
-
-            }
-        });
-
+        api_key = getString(R.string.api_key);
 
         queue = Volley.newRequestQueue(this);
-        get_track = "http://api.musixmatch.com/ws/1.1/track.search?apikey=XXX";
-        get_lyrics = "http://api.musixmatch.com/ws/1.1/track.lyrics.get?apikey=XXX&track_id=";
-
+        get_track = "http://api.musixmatch.com/ws/1.1/track.search?apikey=" + api_key;
+        get_lyrics = "http://api.musixmatch.com/ws/1.1/track.lyrics.get?apikey=" + api_key + "&track_id=";
+//        new GoogleAsyncTask().execute();
         find.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 getTrack();
-                stt();
             }
         });
     }
@@ -170,58 +127,6 @@ public class MainActivity extends AppCompatActivity {
         queue.add(jsonRequest);
     }
 
-    public void stt() {
-
-        new SimpleAsynsTask(user_lyrics).execute();
-    }
-
-
-    public void MediaRecorderReady() {
-        mediaRecorder = new MediaRecorder();
-        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
-        mediaRecorder.setOutputFile(AudioSavePathInDevice);
-    }
-
-    private void requestPermission() {
-        ActivityCompat.requestPermissions(MainActivity.this, new
-                String[]{WRITE_EXTERNAL_STORAGE, RECORD_AUDIO}, RequestPermissionCode);
-    }
-/*
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case RequestPermissionCode:
-                if (grantResults.length > 0) {
-                    boolean StoragePermission = grantResults[0] ==
-                            PackageManager.PERMISSION_GRANTED;
-                    boolean RecordPermission = grantResults[1] ==
-                            PackageManager.PERMISSION_GRANTED;
-
-                    if (StoragePermission && RecordPermission) {
-                        Toast.makeText(MainActivity.this, "Permission Granted",
-                                Toast.LENGTH_LONG).show();
-                    } else {
-                        Toast.makeText(MainActivity.this, "Permission Denied", Toast.LENGTH_LONG).show();
-                    }
-                }
-                break;
-        }
-    }
-*/
-
-    public boolean checkPermission() {
-        int result = ContextCompat.checkSelfPermission(getApplicationContext(),
-                WRITE_EXTERNAL_STORAGE);
-        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(),
-                RECORD_AUDIO);
-        return result == PackageManager.PERMISSION_GRANTED &&
-                result1 == PackageManager.PERMISSION_GRANTED;
-    }
-
     public void getLyrics() {
         {
             get_lyrics_new = get_lyrics + Long.toString(track_id);
@@ -246,163 +151,77 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void convertAudio(View v, File wavFile) {
-        /**
-         *  Update with a valid audio file!
-         *  Supported formats: {@link AndroidAudioConverter.AudioFormat}
-         */
+    public void shot() {
+        Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+        Network network = new BasicNetwork(new HurlStack());
+        RequestQueue queue = new RequestQueue(cache, network);
+        queue.start();
 
-        //File wavFile = new File(Environment.getExternalStorageDirectory(), "recorded_audio.wav");
-        IConvertCallback callback = new IConvertCallback() {
+        String url = "https://speech.googleapis.com/v1/speech:recognize?key=" + getString(R.string.google);
+        try {
+            jsonRequest = new JSONObject(
+
+            "{\n" +
+                    "  \"config\": {\n" +
+                    "  \"sampleRateHertz\": 16000,\n" +
+                    "  \"languageCode\": \"en-US\",\n" +
+                    "  },\n" +
+                    "  \"audio\": {\n" +
+                    "    \"uri\": \"gs://catchmusic/audio-file\"" +
+                    "  }\n" +
+                    "}"
+
+            );
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, jsonRequest, new Response.Listener<JSONObject>() {
             @Override
-            public void onSuccess(File convertedFile) {
-                finalFile = convertedFile;
-                Toast.makeText(MainActivity.this, "SUCCESS: " + convertedFile.getPath(), Toast.LENGTH_LONG).show();
-            }
+            public void onResponse(JSONObject response) {
+                System.out.println(response);
+                try {
+                    String name = response.getString("name");
+                    final String url1 = "https://speech.googleapis.com/v1/operations/" + name + "?key=" + getString(R.string.google);
+                    shot1(url1);
 
-            @Override
-            public void onFailure(Exception error) {
-                Toast.makeText(MainActivity.this, "ERROR: " + error.getMessage(), Toast.LENGTH_LONG).show();
-            }
-        };
-        Toast.makeText(this, "Converting audio file...", Toast.LENGTH_SHORT).show();
-        AndroidAudioConverter.with(this)
-                .setFile(wavFile)
-                .setFormat(AudioFormat.FLAC)
-                .setCallback(callback)
-                .convert();
-    }
-
-    private static final String FRAGMENT_MESSAGE_DIALOG = "message_dialog";
-
-    private static final String STATE_RESULTS = "results";
-
-    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 1;
-
-    private SpeechService mSpeechService;
-
-    private VoiceRecorder mVoiceRecorder;
-    private final VoiceRecorder.Callback mVoiceCallback = new VoiceRecorder.Callback() {
-
-        @Override
-        public void onVoiceStart() {
-            //showStatus(true);
-            if (mSpeechService != null) {
-                mSpeechService.startRecognizing(mVoiceRecorder.getSampleRate());
-            }
-        }
-
-        @Override
-        public void onVoice(byte[] data, int size) {
-            if (mSpeechService != null) {
-                mSpeechService.recognize(data, size);
-            }
-        }
-
-        @Override
-        public void onVoiceEnd() {
-            //showStatus(false);
-            if (mSpeechService != null) {
-                mSpeechService.finishRecognizing();
-            }
-        }
-
-    };
-
-
-    private final ServiceConnection mServiceConnection = new ServiceConnection() {
-
-        @Override
-        public void onServiceConnected(ComponentName componentName, IBinder binder) {
-            mSpeechService = SpeechService.from(binder);
-            mSpeechService.addListener(mSpeechServiceListener);
-//            mStatus.setVisibility(View.VISIBLE);
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName componentName) {
-            mSpeechService = null;
-        }
-
-    };
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        // Prepare Cloud Speech API
-        bindService(new Intent(this, SpeechService.class), mServiceConnection, BIND_AUTO_CREATE);
-
-        // Start listening to voices
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
-                == PackageManager.PERMISSION_GRANTED) {
-            startVoiceRecorder();
-        } else if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                Manifest.permission.RECORD_AUDIO)) {
-            //showPermissionMessageDialog();
-        } else {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO},
-                    REQUEST_RECORD_AUDIO_PERMISSION);
-        }
-    }
-
-    @Override
-    protected void onStop() {
-        // Stop listening to voice
-        stopVoiceRecorder();
-
-        // Stop Cloud Speech API
-        mSpeechService.removeListener(mSpeechServiceListener);
-        unbindService(mServiceConnection);
-        mSpeechService = null;
-
-        super.onStop();
-    }
-
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION) {
-            if (permissions.length == 1 && grantResults.length == 1
-                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                startVoiceRecorder();
-            } else {
-                //showPermissionMessageDialog();
-            }
-        } else {
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-    }
-
-
-    private void startVoiceRecorder() {
-        if (mVoiceRecorder != null) {
-            mVoiceRecorder.stop();
-        }
-        mVoiceRecorder = new VoiceRecorder(mVoiceCallback);
-        mVoiceRecorder.start();
-    }
-
-    private void stopVoiceRecorder() {
-        if (mVoiceRecorder != null) {
-            mVoiceRecorder.stop();
-            mVoiceRecorder = null;
-        }
-    }
-
-    private final SpeechService.Listener mSpeechServiceListener =
-            new SpeechService.Listener() {
-                @Override
-                public void onSpeechRecognized(final String text, final boolean isFinal) {
-                    if (isFinal) {
-                        mVoiceRecorder.dismiss();
-                        Log.i("onSpeechRecognized: ", text);
-                    }
-
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
-            };
 
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });
+        queue.add(jsonObjectRequest);
     }
+
+    public void shot1(String url1) {
+
+        Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+        Network network = new BasicNetwork(new HurlStack());
+        RequestQueue queue = new RequestQueue(cache, network);
+        queue.start();
+
+        final JsonObjectRequest jsonObjectRequest1 = new JsonObjectRequest(Request.Method.GET, url1, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(final JSONObject response) {
+
+                System.out.println(response);
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });
+        jsonObjectRequest1.setRetryPolicy(new DefaultRetryPolicy(DefaultRetryPolicy.DEFAULT_TIMEOUT_MS * 3, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        queue.add(jsonObjectRequest1);
+    }
+
+
+}
 
 
