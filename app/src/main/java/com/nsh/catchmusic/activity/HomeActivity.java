b80ae490bc30027e7be549cd45e002f9dc9b1d28
ThisIsNSH <hadanis.singh@gmail.com>
2018-06-10 16:41:24 +0530

    done

--- app/src/main/java/com/nsh/catchmusic/activity/HomeActivity.java
+++ app/src/main/java/com/nsh/catchmusic/activity/HomeActivity.java
@@ -2,10 +2,12 @@ package com.nsh.catchmusic.activity;
 
 import android.animation.ObjectAnimator;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
@@ -16,6 +18,8 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.Animation;
@@ -47,6 +51,7 @@ import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -58,7 +63,7 @@ public class HomeActivity extends AppCompatActivity {
     RecyclerView rec_singer, rec_album;
     TextView name, album, singer;
     ImageView imageView;
-    FrameLayout button;
+    FrameLayout button,lyrics;
     Song1Adapter singerAdapter;
     SongAdapter albumAdapter;
     List<Song> singerList, albumList;
@@ -125,6 +130,41 @@ public class HomeActivity extends AppCompatActivity {
 
                     check = 1;
                 }
+                if (check == 2) {
+                    Animation animate = AnimationUtils.loadAnimation(HomeActivity.this, R.anim.translate_left2);
+                    card.startAnimation(animate);
+                    ObjectAnimator animation = ObjectAnimator.ofFloat(card, "rotationY", -10.0f, 0f);
+                    animation.setDuration(300);
+                    animation.setInterpolator(new AccelerateDecelerateInterpolator());
+                    animation.start();
+
+
+                    Animation animate1 = AnimationUtils.loadAnimation(HomeActivity.this, R.anim.translate_right3);
+                    lyrics.startAnimation(animate1);
+                    ObjectAnimator animation1 = ObjectAnimator.ofFloat(lyrics, "translationX", -130.0f, 0f);
+                    animation1.setDuration(300);
+                    animation1.setInterpolator(new AccelerateDecelerateInterpolator());
+                    animation1.start();
+                    animate1.setAnimationListener(new Animation.AnimationListener() {
+                        @Override
+                        public void onAnimationStart(Animation animation) {
+
+
+                        }
+
+                        @Override
+                        public void onAnimationEnd(Animation animation) {
+                            lyrics.setVisibility(View.GONE);
+                        }
+
+                        @Override
+                        public void onAnimationRepeat(Animation animation) {
+
+                        }
+                    });
+
+                    check = 0;
+                }
             }
 
             public void onSwipeRight() {
@@ -161,10 +201,51 @@ public class HomeActivity extends AppCompatActivity {
 
                     check = 0;
                 }
+                else if(check ==0 ){
+                    Animation animate = AnimationUtils.loadAnimation(HomeActivity.this, R.anim.translate_right2);
+                    card.startAnimation(animate);
+                    ObjectAnimator animation = ObjectAnimator.ofFloat(card, "rotationY", 0f, -10f);
+                    animation.setDuration(300);
+                    animation.setInterpolator(new AccelerateDecelerateInterpolator());
+                    animation.start();
+
+                    Animation animate1 = AnimationUtils.loadAnimation(HomeActivity.this, R.anim.translate_left3);
+                    lyrics.startAnimation(animate1);
+                    ObjectAnimator animation1 = ObjectAnimator.ofFloat(lyrics, "translationX", 0f, -130f);
+                    animation1.setDuration(300);
+                    animation1.setInterpolator(new AccelerateDecelerateInterpolator());
+                    animation1.start();
+                    animate1.setAnimationListener(new Animation.AnimationListener() {
+                        @Override
+                        public void onAnimationStart(Animation animation) {
+                            lyrics.setVisibility(View.INVISIBLE);
+                        }
+
+                        @Override
+                        public void onAnimationEnd(Animation animation) {
+                            lyrics.setVisibility(View.VISIBLE);
+                        }
+
+                        @Override
+                        public void onAnimationRepeat(Animation animation) {
+
+                        }
+                    });
+
+                    check = 2;
+                }
             }
 
 
         });
+
+        lyrics.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new BAsyncTask().execute();
+            }
+        });
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -275,6 +356,7 @@ public class HomeActivity extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
             getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         again = findViewById(R.id.again);
+        lyrics = findViewById(R.id.lyrics);
         blurImageView = findViewById(R.id.BlurImageView);
         holder = findViewById(R.id.holder);
         button = findViewById(R.id.play);
@@ -360,6 +442,39 @@ public class HomeActivity extends AppCompatActivity {
         protected void onPostExecute(Element content) {
             Picasso.get().load(content.select("img").first().absUrl("src")).into(imageView);
             Picasso.get().load(content.select("img").first().absUrl("src")).into(blurImageView);
+            super.onPostExecute(content);
+        }
+    }
+
+    public class BAsyncTask extends AsyncTask<Void, Void, Elements> {
+
+        @Override
+        protected Elements doInBackground(Void... voids) {
+            Elements content = new Elements();
+            try {
+                Document document = Jsoup.connect(track_pic).get();
+                content = document.select(".mxm-lyrics__content");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return content;
+        }
+
+        @Override
+        protected void onPostExecute(Elements content) {
+            Dialog dialog = new Dialog(HomeActivity.this);
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            dialog.setContentView(R.layout.lyrics_card);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+            dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            dialog.show();
+            TextView tv= dialog.findViewById(R.id.lyrics_text);
+            String lyrics = new String();
+            lyrics = "Lyrics";
+            for (Element element : content) {
+            lyrics += "\n\n"+element.html();
+            }
+            tv.setText(lyrics);
             super.onPostExecute(content);
         }
     }
